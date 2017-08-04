2825168aee47c44f2ab8c9473f5291f0e1676f99
Brooks Becton <broabect@ut.utm.edu>
2017-08-03 21:00:55 -0500

    added redux dev tools

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,23 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import { Router, browserHistory } from 'react-router';
-import ReduxPromise from 'redux-promise';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import { Router, browserHistory } from "react-router";
+import ReduxPromise from "redux-promise";
 
-import reducers from './reducers';
-import routes from './routes';
+import reducers from "./reducers";
+import routes from "./routes";
 
-import 'bootstrap-social';
+import "bootstrap-social";
 
 // for bundling your styles
-import './bundle.scss';
+import "./bundle.scss";
+const middlewares = [ReduxPromise];
 
-const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
+const enhancers = [applyMiddleware(...middlewares)];
 
+const composeEnhancers =
+  process.env.NODE_ENV !== "production" &&
+  typeof window === "object" &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    : compose;
+
+const store = createStore(reducers, composeEnhancers(...enhancers));
 
 ReactDOM.render(
-    <Provider store={createStoreWithMiddleware(reducers)}>
-        <Router history={browserHistory} routes={routes} />
-    </Provider>
-  , document.querySelector('.react-root'));
+  <Provider store={store}>
+    <Router history={browserHistory} routes={routes} />
+  </Provider>,
+  document.querySelector(".react-root")
+);
