1498f22e42dfdd43ce26ea69cb10e6b3ddbe4a96
Brooks Becton <broabect@ut.utm.edu>
2017-08-20 19:21:42 -0500

    Added link to jumbotron button

--- src/app/components/index_home.jsx
+++ src/app/components/index_home.jsx
@@ -1,17 +1,17 @@
 import React from "react";
+import { Link } from "react-router";
 
-export default () => {
-  return (
-    <div>
-      <div className="jumbotron">
-        <h1>Doc Bot Proverbs</h1>
-        <h2>Today's Proverb</h2>
-        <p>People who live in glass houses, sink ships.</p>
+export default () =>
+  <div>
+    <div className="jumbotron">
+      <h1>Doc Bot Proverbs</h1>
+      <h2>Today's Proverb</h2>
+      <p>People who live in glass houses, sink ships.</p>
 
+      <Link to={"proverbs/submit"}>
         <button className="btn btn-primary btn-lg" role="button">
           Submit a Proverb
         </button>
-      </div>
+      </Link>
     </div>
-  );
-};
+  </div>;
